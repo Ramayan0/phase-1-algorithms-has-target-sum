23428928b9a72c12eafdca3d351640316c93f444
hamsa <hafieekhan@gmail.com>
2022-08-27 14:15:41 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for (let i = 0; i < array.length; i++){
+    const complement = target - array[i];
+    for (let j = i +1; j < array.length; j++){
+      if(array[j] === complement){
+        return true;
+      }      
+    }
+  }
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+  time complexity = O(n²) quadratic
+
 */
 
 /* 
@@ -12,6 +23,7 @@ function hasTargetSum(array, target) {
 
 /*
   Add written explanation of your solution here
+
 */
 
 // You can run `node index.js` to view these console logs
